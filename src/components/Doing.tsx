b1f2a86e86b35117999b14a3526d826676a64369
Lorraine Sanares <96808637+Raine0554@users.noreply.github.com>
2025-01-11 02:07:03 +1100

    cleaned up code in Doing file

--- src/components/Doing.tsx
+++ src/components/Doing.tsx
@@ -12,25 +12,22 @@ const Doing: React.FC = () => {
             <div className="flex flex-row items-start justify-center w-full gap-10">
                 {/* Study Card */}
                 <div className="flex flex-col w-1/3 h-[350] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md">
-                    {/* Heading */}
+                    {/* Text */}
                     <div className="text-4xl sm:text-4xl font-bold text-black">Studying</div>
-                    {/* Body Text */}
                     <p className="text-lg sm:text-xl text-black mb-6 text-left">Helloo</p>
                 </div>
 
                 {/* Coding Card */}
                 <div className="flex flex-col w-1/3 h-[350] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md">
-                    {/* Heading */}
+                    {/* Text */}
                     <div className="text-4xl sm:text-4xl font-bold text-black">Coding</div>
-                    {/* Body Text */}
                     <p className="text-lg sm:text-xl text-black mb-6 text-left">Helloo</p>
                 </div>
                 
                 {/* Writing Card */}
                 <div className="flex flex-col w-1/3 h-[350] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md">
-                    {/* Heading */}
-                    <div className="text-4xl sm:text-4xl font-bold text-black">Writing</div>
-                    {/* Body Text */}
+                    {/* Text */}
+                    <div className="text-4xl sm:text-4xl font-bold text-black">Writing</div>            
                     <p className="text-lg sm:text-xl text-black mb-6 text-left">Helloo</p>
                 </div>                
             </div>
