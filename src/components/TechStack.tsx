328aff8db103a3cdd2db399dbc87686eed5b8fe2
Lorraine Sanares <96808637+Raine0554@users.noreply.github.com>
2025-01-16 16:20:17 +1100

    Tech stack description

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -7,6 +7,19 @@ const TechStack: React.FC = () => {
             <h1 className="text-4xl sm:text-6xl font-extrabold mb-4 text-white">
                     Tech Stack
             </h1>
+
+            {/* Description Text */}
+            <div className="flex flex-col items-start justify-start w-1/2 h-auto">
+                <p>
+                On top of building my proficiency in programming languages for analytics 
+                (i.e Python and R), I also like to dabble in UX/UI design, web development, 
+                data engineering and cloud computing. <br />
+
+                <br /> More details of my tech skills can be viewed on my LinkedIn and GitHub!
+                </p>
+            </div>
+
+
         </div> 
     );
 
