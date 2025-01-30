5e6ec72c5a8ae2b808f9f71bb4a7d2a97f5c17b4
Lorraine Sanares <96808637+Raine0554@users.noreply.github.com>
2025-01-30 14:11:04 +1100

    added animations, icons and text to cards

--- src/components/Doing.tsx
+++ src/components/Doing.tsx
@@ -15,7 +15,9 @@ const Doing: React.FC = () => {
             {/* Cards Container */} 
             <div className="flex flex-row items-start justify-center w-full gap-10">
                 {/* Study Card */}
-                <div className="relative flex flex-col w-1/3 h-auto rounded-3xl bg-white p-10 bg-opacity-80 backdrop-blur-md overflow-hidden gap-4">
+                <div className="relative flex flex-col w-1/3 h-[550] rounded-3xl bg-white 
+                                p-6 bg-opacity-80 backdrop-blur-md overflow-hidden gap-4 
+                                hover:bg-[#C0CAFF] transition-transform duration-300 hover:scale-105">
                     {/* Title at the Top Right */}
                     <div className="absolute top-6 right-6 text-4xl sm:text-2xl pb-6 font-bold text-black text-right">
                         CONTINUOUS<br /> LEARNING
@@ -24,11 +26,11 @@ const Doing: React.FC = () => {
                     <div className="container"></div>
 
                     {/* Image in the Center */}
-                    <div className="flex justify-center items-center flex-grow">
+                    <div className="flex justify-center items-center pt-1">
                         <Image 
                             src="/assets/book.png" 
                             alt="Overlay" 
-                            width={500} 
+                            width={400} 
                             height={100} 
                             style={{ objectFit: "contain" }} 
                             className="rounded-lg"
@@ -36,22 +38,25 @@ const Doing: React.FC = () => {
                     </div>
 
                     {/* Text Paragraph Below the Image */}
-                    <p className="text-lg sm:text-xl text-black mb-4 text-left">
-                        In my spare time, I love to code
+                    <p className="text-lg sm:text-md text-black mb-4 text-left leading-tight">
+                        I use writing to shape ideas, share insights, and spark 
+                        conversations. It helps me distill complex thoughts, 
+                        communicate effectively, and inspire action—whether through 
+                        technical explanations, storytelling, or reflective pieces.
                     </p>
                 </div>
 
                 {/* Coding Card */}
-                <div className="relative flex flex-col w-1/3 h-auto rounded-3xl bg-white p-10 bg-opacity-80 backdrop-blur-md overflow-hidden gap-4">
+                <div className="relative flex flex-col w-1/3 h-[550] rounded-3xl bg-white p-6 bg-opacity-80 backdrop-blur-md overflow-hidden gap-4 hover:bg-[#C0CAFF] transition-transform duration-300 hover:scale-105">
                     {/* Title at the Top Right */}
                     <div className="absolute top-6 right-6 text-4xl sm:text-2xl pb-6 font-bold text-black text-right">
-                        CREATIVE<br /> IDEATION
+                        CREATIVE<br /> LOGIC
                     </div>
 
                     <div className="container"></div>
 
                     {/* Image in the Center */}
-                    <div className="flex justify-center items-center flex-grow">
+                    <div className="flex justify-center items-center pt-14">
                         <Image 
                             src="/assets/king.png" 
                             alt="Overlay" 
@@ -63,27 +68,30 @@ const Doing: React.FC = () => {
                     </div>
 
                     {/* Text Paragraph Below the Image */}
-                    <p className="text-lg sm:text-xl text-black mb-4 text-left">
-                        In my spare time, I love to code
+                    <p className="text-lg sm:text-md text-black pt-4 mb-4 text-left leading-tight">
+                        I use writing to shape ideas, share insights, and spark 
+                        conversations. It helps me distill complex thoughts, 
+                        communicate effectively, and inspire action—whether through 
+                        technical explanations, storytelling, or reflective pieces.
                     </p>
                 </div>
 
                 
                 {/* Writing Card */}
-                <div className="relative flex flex-col w-1/3 h-auto rounded-3xl bg-white p-10 bg-opacity-80 backdrop-blur-md overflow-hidden gap-4">
+                <div className="relative flex flex-col w-1/3 h-[550] rounded-3xl bg-white p-6 bg-opacity-80 backdrop-blur-md overflow-hidden gap-6 hover:bg-[#C0CAFF] transition-transform duration-300 hover:scale-105">
                     {/* Title at the Top Right */}
                     <div className="absolute top-6 right-6 text-4xl sm:text-2xl pb-6 font-bold text-black text-right">
-                        WRITING<br /> TO LEARN
+                        WRITING TO<br />INFLUENCE
                     </div>
 
                     <div className="container"></div>
 
                     {/* Image in the Center */}
-                    <div className="flex justify-center items-center flex-grow">
+                    <div className="flex justify-center items-center pt-14">
                         <Image 
                             src="/assets/pen.png" 
                             alt="Overlay" 
-                            width={200} 
+                            width={180} 
                             height={100} 
                             style={{ objectFit: "contain" }} 
                             className="rounded-lg"
@@ -91,8 +99,11 @@ const Doing: React.FC = () => {
                     </div>
 
                     {/* Text Paragraph Below the Image */}
-                    <p className="text-lg sm:text-xl text-black mb-4 text-left">
-                        In my spare time, I love to code
+                    <p className="text-lg sm:text-md text-black pt-4 mb-4 text-left leading-tight">
+                        I use writing to shape ideas, share insights, and spark 
+                        conversations. It helps me distill complex thoughts, 
+                        communicate effectively, and inspire action—whether through 
+                        technical explanations, storytelling, or reflective pieces.
                     </p>
                 </div>               
             </div>
