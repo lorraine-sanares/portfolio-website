342455f8991b7f9f4e518195c4990cabd6994664
Lorraine Sanares <96808637+Raine0554@users.noreply.github.com>
2025-02-02 22:12:22 +1100

    fixed sticky heading and image

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useRef, useEffect, useState } from "react";
 
-// MAKE THIS SECTION SCROLLABLE, LOCK THE BACKGROUND
+// DOESN'T WORK FOR LARGE SCREENS
 
 const Experience: React.FC = () => {
     const containerRef = useRef<HTMLDivElement>(null); // Reference to the container
@@ -26,93 +26,110 @@ const Experience: React.FC = () => {
 
     return (
         <section
-            style={{
-                backgroundImage: "url('/assets/stack.png')",
-                backgroundSize: "80%",
-                backgroundPosition: "-90% 30%",
-                backgroundRepeat: "no-repeat", 
-                backgroundAttachment: "fixed"
-            }}
+            // style={{
+            //     backgroundImage: "url('/assets/stack.png')",
+            //     backgroundSize: "80%",
+            //     backgroundPosition: "-90% 30%",
+            //     backgroundRepeat: "no-repeat", 
+            //     backgroundAttachment: "fixed"
+            // }}
             className="relative w-full h-auto text-white"
         >
-            <div className="flex flex-col items-start justify-start w-full h-auto pl-28">
-                {/* Heading */}
-                <h1 className="text-4xl sm:text-6xl font-extrabold mb-4 text-white sticky top-0 pt-28">
-                    Work Experience
-                </h1>
-
-                {/* Content Container */}
-                <div className="flex flex-row items-start w-full h-auto mt-25 " ref={containerRef}>
-                    {/* Spacer */}
-                    <div className="container w-1/2"></div>
-
-                    {/* Vertical Line with Square and Circles */}
-                    <div className="relative flex flex-col items-center h-screen w-[50px]">
-                        {/* Square */}
-                        <div className="w-[50px] h-[50px] rounded-xl bg-white bg-opacity-80"></div>
-
-                        {/* Vertical Line */}
-                        <div className="relative border-l border-white h-full"></div>
-
-                        {/* Circles aligned with cards */}
-                        {circlePositions.map((position, index) => (
-                            <div
-                                key={index}
-                                className="absolute w-[20px] h-[20px] rounded-full bg-white"
-                                style={{ top: `${position + 20}px` }}
-                            ></div>
-                        ))}
-                    </div>
-
-                    {/* Experience Cards */}
-                    <div className="flex flex-col items-start justify-start w-auto h-auto p-16 gap-8">
-                        <div
-                            ref={cardRefs[0]}
-                            className="flex flex-col w-[600px] h-[350px] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md"
-                        >
-                            <div className="text-3xl font-bold text-black">AI & Data Intern At Deloitte</div>
-                            <p className="text-lg sm:text-xl text-black mb-6 text-left">
-                                Nov. 2024 - Dec. 2024
-                            </p>
-                        </div>
-
-                        <div
-                            ref={cardRefs[1]}
-                            className="flex flex-col w-[600px] h-[350px] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md"
-                        >
-                            <div className="text-3xl font-bold text-black">Private Piano Teacher</div>
-                            <p className="text-lg sm:text-xl text-black mb-6 text-left">
-                                Nov. 2024 - Dec. 2024
-                            </p>
-                        </div>
-
-                        <div
-                            ref={cardRefs[2]}
-                            className="flex flex-col w-[600px] h-[350px] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md"
-                        >
-                            <div className="text-3xl font-bold text-black">Teaching Assistant At Kumon</div>
-                            <p className="text-lg sm:text-xl text-black mb-6 text-left">
-                                Nov. 2024 - Dec. 2024
-                            </p>
-                        </div>
-                        
-                        <div
-                            ref={cardRefs[3]}
-                            className="flex flex-col w-[600px] h-[350px] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md"
-                        >
-                            <div className="text-3xl font-bold text-black">Private Maths Tutor</div>
-                            <p className="text-lg sm:text-xl text-black mb-6 text-left">
-                                Mar. 2022 - Oct. 2022
-                            </p>
-                        </div>
-                        
-                    </div>
+            
+            <div className="flex flex-row items-start justify-start w-full h-auto">
+                {/* Left Column - Heading and Image */}
+                <div className="flex flex-col items-start justify-start w-full h-auto pt-10 pl-20 sticky top-0">
+                    {/* Heading */}
+                    <h1 className="text-4xl sm:text-6xl font-extrabold mb-4 text-white">
+                        Work Experience
+                    </h1>
                     
-                </div>
-                
-                
+                    <div className="relative top-0 left-0 w-full h-screen bg-cover bg-no-repeat"
+                        style={{
+                            backgroundImage: "url('/assets/stack.png')",
+                            backgroundSize: "160%",
+                            backgroundPosition: "40% -200%",
+                            // zIndex: -1, // Push background behind content
+                        }}
+                    ></div>
 
+                </div>
+                {/* Right Column - Content Container */}
+                <div className="flex flex-col items-start justify-start w-full h-auto pt-28 pl-28">
+                    
+                    <div className="flex flex-row items-start w-full h-auto mt-25 " ref={containerRef}>
+                        {/* Spacer */}
+                        <div className="container w-1/2"></div>
+
+                        {/* Vertical Line with Square and Circles */}
+                        <div className="relative flex flex-col items-center h-screen w-[50px]">
+                            {/* Square */}
+                            <div className="w-[50px] h-[50px] rounded-xl bg-white bg-opacity-80"></div>
+
+                            {/* Vertical Line */}
+                            <div className="relative border-l border-white h-full"></div>
+
+                            {/* Circles aligned with cards */}
+                            {circlePositions.map((position, index) => (
+                                <div
+                                    key={index}
+                                    className="absolute w-[20px] h-[20px] rounded-full bg-white"
+                                    style={{ top: `${position + 20}px` }}
+                                ></div>
+                            ))}
+                        </div>
+
+                        {/* Experience Cards */}
+                        <div className="flex flex-col items-start justify-start w-auto h-auto p-16 gap-8">
+                            <div
+                                ref={cardRefs[0]}
+                                className="flex flex-col w-[600px] h-[350px] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md"
+                            >
+                                <div className="text-3xl font-bold text-black">AI & Data Intern At Deloitte</div>
+                                <p className="text-lg sm:text-xl text-black mb-6 text-left">
+                                    Nov. 2024 - Dec. 2024
+                                </p>
+                            </div>
+
+                            <div
+                                ref={cardRefs[1]}
+                                className="flex flex-col w-[600px] h-[350px] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md"
+                            >
+                                <div className="text-3xl font-bold text-black">Private Piano Teacher</div>
+                                <p className="text-lg sm:text-xl text-black mb-6 text-left">
+                                    Nov. 2024 - Dec. 2024
+                                </p>
+                            </div>
+
+                            <div
+                                ref={cardRefs[2]}
+                                className="flex flex-col w-[600px] h-[350px] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md"
+                            >
+                                <div className="text-3xl font-bold text-black">Teaching Assistant At Kumon</div>
+                                <p className="text-lg sm:text-xl text-black mb-6 text-left">
+                                    Nov. 2024 - Dec. 2024
+                                </p>
+                            </div>
+                            
+                            <div
+                                ref={cardRefs[3]}
+                                className="flex flex-col w-[600px] h-[350px] rounded-3xl bg-white p-10 text-start bg-opacity-80 backdrop-blur-md"
+                            >
+                                <div className="text-3xl font-bold text-black">Private Maths Tutor</div>
+                                <p className="text-lg sm:text-xl text-black mb-6 text-left">
+                                    Mar. 2022 - Oct. 2022
+                                </p>
+                            </div>
+                            
+                        </div>
+                            
+                    </div>
+                </div>
             </div>
+
+            
+
+
         </section>
     );
 };
@@ -141,13 +158,13 @@ const Experience: React.FC = () => {
             
 //             <div></div>
 //             {/* Background Image Container */}
-//             <div className="absolute inset-0 w-full h-full" style={{
-//                 backgroundImage: "url('/assets/stack.png')",
-//                 backgroundSize: "150%",
-//                 backgroundPosition: "130% 25%",
-//                 backgroundRepeat: "no-repeat",
-//                 backgroundAttachment: "fixed"
-//             }}></div>
+            // <div className="absolute inset-0 w-full h-full" style={{
+            //     backgroundImage: "url('/assets/stack.png')",
+            //     backgroundSize: "150%",
+            //     backgroundPosition: "130% 25%",
+            //     backgroundRepeat: "no-repeat",
+            //     backgroundAttachment: "fixed"
+            // }}></div>
             
 //             <div className="flex flex-col items-start justify-start w-full h-auto pl-28">
 //                 {/* Heading */}
